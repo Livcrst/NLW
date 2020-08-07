e4696d4c0429d7498511ce423a2e946f474f61d3
Livia Nascimento <livcristinna@gmail.com>
2020-08-06 21:07:38 -0300

    Interface + comeco de backend

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,3 +1,24 @@
-<script>
-       
- </script>
+
+//Procurar botão
+        document.querySelector("#add-time")
+        .addEventListener('click', cloneField)
+
+//Quando clicar no botão
+// executar ação
+function cloneField(){
+        // Duplicar os campos
+        const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+
+        // Antes de colocar na page, limpar os campos: Que campos?
+        const fields = newFieldContainer.querySelectorAll('input')        // colocar na página: Onde?
+
+        //Limpar para cada campo 
+        fields.forEach(function(field) {
+                //pegar o field do momento
+                field.value=""
+        });
+        //Onde por na page
+        document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+         
+         
